0d1defc8a42c39a901e3dbabcb81fb86ce3a80a6
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-10 17:22:40 +0300

    Remove bottom navigation from main activity

--- app/src/main/java/com/itmax/chatapp/MainActivity.java
+++ app/src/main/java/com/itmax/chatapp/MainActivity.java
@@ -2,14 +2,10 @@ package com.itmax.chatapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.Menu;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -50,38 +46,30 @@ public class MainActivity extends AppCompatActivity {
             NavigationUI.setupWithNavController(navigationView, navController);
         }
 
-        BottomNavigationView bottomNavigationView = binding.appBarMain.contentMain.bottomNavView;
-        if (bottomNavigationView != null) {
-            mAppBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.nav_chats, R.id.nav_chat, R.id.nav_slideshow)
-                    .build();
-            NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-            NavigationUI.setupWithNavController(bottomNavigationView, navController);
-        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        boolean result = super.onCreateOptionsMenu(menu);
-        // Using findViewById because NavigationView exists in different layout files
-        // between w600dp and w1240dp
-        NavigationView navView = findViewById(R.id.nav_view);
-        if (navView == null) {
-            // The navigation drawer already has the items including the items in the overflow menu
-            // We only inflate the overflow menu if the navigation drawer isn't visible
-            getMenuInflater().inflate(R.menu.overflow, menu);
-        }
-        return result;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        boolean result = super.onCreateOptionsMenu(menu);
+//        // Using findViewById because NavigationView exists in different layout files
+//        // between w600dp and w1240dp
+//        NavigationView navView = findViewById(R.id.nav_view);
+//        if (navView == null) {
+//            // The navigation drawer already has the items including the items in the overflow menu
+//            // We only inflate the overflow menu if the navigation drawer isn't visible
+//            getMenuInflater().inflate(R.menu.overflow, menu);
+//        }
+//        return result;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.nav_settings) {
-            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-            navController.navigate(R.id.nav_settings);
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if (item.getItemId() == R.id.nav_settings) {
+//            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//            navController.navigate(R.id.nav_settings);
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     public boolean onSupportNavigateUp() {
