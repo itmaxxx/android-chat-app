bb38cd08957d0251f20227f369e5040607f65de6
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-11 00:28:41 +0300

    Create message model

---
+++ app/src/main/java/com/itmax/chatapp/data/model/Message.java
@@ -0,0 +1,68 @@
+package com.itmax.chatapp.data.model;
+
+public class Message {
+
+    private String id;
+    private String chatId;
+    private String text;
+    private User author;
+    private int createdAt;
+    private int updatedAt;
+
+    Message(String id, String chatId, String text, String author, int createdAt, int updatedAt) {
+        this.id = id;
+        this.chatId = chatId;
+        this.text = text;
+        this.author = author;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public int getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(int createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(int updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
