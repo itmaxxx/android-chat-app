99c2665734fbd9317fa8cdb1f57f0e144fb5773f
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-12 00:10:37 +0300

    Get chat messages in chat view model

--- app/src/main/java/com/itmax/chatapp/ui/chat/ChatViewModel.java
+++ app/src/main/java/com/itmax/chatapp/ui/chat/ChatViewModel.java
@@ -4,16 +4,36 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.itmax.chatapp.data.Result;
+import com.itmax.chatapp.data.model.Message;
+import com.itmax.chatapp.data.repositories.ChatRepository;
+
+import java.util.List;
+
 public class ChatViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final ChatRepository chatRepository;
+    private final MutableLiveData<List<Message>> messagesList;
 
-    public ChatViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is chat fragment from code");
+    public ChatViewModel(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+        messagesList = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void loadChatMessages(String chatId) {
+        Runnable loadChats = () -> {
+            Result<List<Message>> result = chatRepository.getChatMessages(chatId);
+
+            if (result instanceof Result.Success) {
+                List<Message> data = ((Result.Success<List<Message>>) result).getData();
+                messagesList.postValue(data);
+            }
+        };
+
+        loadChats.run();
+    }
+
+    public LiveData<List<Message>> getChatMessages() {
+        return this.messagesList;
     }
 }
